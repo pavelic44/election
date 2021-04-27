6f7729a45e2c1059edd2c0883db11a5a9c29c98b
Andrej Pavelić <apavelic@erecruit.com>
2021-04-27 19:29:05 +0200

    adding cookies notification

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,15 @@
 (function ($) {
     "use strict";
 
+    $(document).ready(function(){   
+        setTimeout(function () {
+            $("#cookieConsent").fadeIn(200);
+         }, 4000);
+        $("#closeCookieConsent, .cookieConsentOK").click(function() {
+            $("#cookieConsent").fadeOut(200);
+        }); 
+    }); 
+
     /*:::::::::::::::::::::::::::::::::::
             Navbar Area
     :::::::::::::::::::::::::::::::::::*/
